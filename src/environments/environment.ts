fbd8288a61c977a86f521c5311d2f10113200e95
szymon <szymonstuszek@gmail.com>
2025-05-05 22:32:54 +0200

    test connection with api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
+// TODO setup switch between local and prod
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://10x-mtg-deckbuilder-api.com'
 }; 
